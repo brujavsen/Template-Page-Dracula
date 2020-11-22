98813ca8d5a0cd2574434317b51bdedb1ae51ebc
brujavsen <68858114+brujavsen@users.noreply.github.com>
2020-11-22 15:28:42 -0300

    dracula template

---
+++ js/init.js
@@ -0,0 +1,44 @@
+const showPassword = document.getElementById('show-pass');
+const inputUser = document.getElementById('login-user');
+const inputPass = document.getElementById('login-pass');
+const btnForm = document.getElementById('btn-form');
+const form = document.getElementById('login-form');
+
+//Change the color of body
+function colorChange() {
+    var bodyPage = document.body;
+    bodyPage.style.backgroundColor = bodyPage.style.backgroundColor == "black" ? "crimson" : "black"; 
+}
+
+//Toggle button text
+function btnChange(){
+    const value = btnForm.getAttribute('value') == "Let's go" ? "Ready to die?" : "Let's go";
+    btnForm.setAttribute("value", value);
+}
+
+showPassword.addEventListener('click', ()=>{
+    const type = inputPass.getAttribute('type') === "password" ? "text" : "password";
+    inputPass.setAttribute("type", type);
+
+    this.classList.toggle("fa fa-slash");
+})
+
+//timer
+var tmp = setInterval(()=>{
+    colorChange();
+    btnChange();
+},2000);
+
+console.log(inputUser.length);
+form.addEventListener('submit', (e)=>{
+
+    var existErr = false;
+    if(inputUser.value == "" || inputPass.value == ""){
+        alert("Please, fill in the empty boxes.\n Or Dracula will come for you")
+        existErr = true;
+    }
+
+    if(existErr){
+        e.preventDefault();
+    }
+})
